49697a2c0c74c17d89cbe5452b847be47738cf45
Leon Tan <leon.arian.tan@gmail.com>
2025-06-28 11:23:19 +0200

    Parsing works

--- luct-core/src/lib.rs
+++ luct-core/src/lib.rs
@@ -1,1 +1,34 @@
+use crate::utils::base64::Base64;
+use serde::{Deserialize, Serialize};
+use url::Url;
 
+pub(crate) mod utils;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct CtLog {
+    url: Url,
+    log_id: Base64<Vec<u8>>,
+    key: Base64<Vec<u8>>,
+    mdd: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ARGON2025H1: &str = "
+url = \"https://ct.googleapis.com/logs/us1/argon2025h1/\"
+log_id = \"TnWjJ1yaEMM4W2zU3z9S6x3w4I4bjWnAsfpksWKaOd8=\"
+key = \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEIIKh+WdoqOTblJji4WiH5AltIDUzODyvFKrXCBjw/Rab0/98J4LUh7dOJEY7+66+yCNSICuqRAX+VPnV8R1Fmg==\"
+mdd = 86400
+    ";
+
+    fn get_log() -> CtLog {
+        toml::from_str(ARGON2025H1).unwrap()
+    }
+
+    #[test]
+    fn ct_log_tom_parses() {
+        let _ = get_log();
+    }
+}
---
+++ luct-core/src/utils/base64.rs
@@ -0,0 +1,43 @@
+use base64::{Engine, prelude::BASE64_STANDARD};
+use serde::{Deserialize, Serialize, Serializer};
+use std::ops::{Deref, DerefMut};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) struct Base64<T>(pub T);
+
+impl<T> Deref for Base64<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Base64<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T: AsRef<[u8]>> Serialize for Base64<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let encoded = BASE64_STANDARD.encode(self.0.as_ref());
+        serializer.serialize_str(&encoded)
+    }
+}
+
+impl<'de, T: From<Vec<u8>>> Deserialize<'de> for Base64<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let encoded = <String>::deserialize(deserializer)?;
+        BASE64_STANDARD
+            .decode(encoded)
+            .map_err(serde::de::Error::custom)
+            .map(|v| Base64(T::from(v)))
+    }
+}
