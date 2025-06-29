13ee6ddccef919294405d2f8c448eaae723ee1bd
Leon Tan <leon.arian.tan@gmail.com>
2025-06-29 13:05:09 +0200

    WIP building sct

--- luct-core/src/v1/sct.rs
+++ luct-core/src/v1/sct.rs
@@ -1,4 +1,13 @@
-use crate::{Version, utils::vec::CodecVec, v1::LogEntry};
+use crate::{
+    Version,
+    utils::{
+        codec::{CodecError, Decode, Encode},
+        signature::Signature,
+        vec::CodecVec,
+    },
+    v1::{LogEntry, SignatureType},
+};
+use std::io::{Read, Write};
 
 /// See RFC 6962 3.2
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -6,7 +15,7 @@ pub struct SignedCertificateTimestamp {
     sct_version: Version,
     id: [u8; 32],
     extensions: CodecVec<u16>,
-    // TODO: Signature of Certificate time stamp
+    signature: Signature<CertificateTimeStamp>,
 }
 
 /// See RFC 6962 3.2
@@ -17,4 +26,36 @@ pub(crate) struct CertificateTimeStamp {
     timestamp: u64,
     entry: LogEntry,
     extensions: CodecVec<u16>,
+}
+
+impl Encode for CertificateTimeStamp {
+    fn encode(&self, mut writer: impl Write) -> Result<(), CodecError> {
+        self.sct_version.encode(&mut writer)?;
+        SignatureType::CertificateTimeStamp.encode(&mut writer)?;
+        self.timestamp.encode(&mut writer)?;
+        self.entry.encode(&mut writer)?;
+        self.extensions.encode(&mut writer)?;
+        Ok(())
+    }
+}
+
+impl Decode for CertificateTimeStamp {
+    fn decode(mut reader: impl Read) -> Result<Self, CodecError> {
+        let sct_version = Version::decode(&mut reader)?;
+        let signature_type = SignatureType::decode(&mut reader)?;
+        match signature_type {
+            SignatureType::TreeHash => return Err(CodecError::UnexpectedVariant),
+            SignatureType::CertificateTimeStamp => (),
+        }
+        let timestamp = u64::decode(&mut reader)?;
+        let entry = LogEntry::decode(&mut reader)?;
+        let extensions = CodecVec::decode(&mut reader)?;
+
+        Ok(Self {
+            sct_version,
+            timestamp,
+            entry,
+            extensions,
+        })
+    }
 }
