c3c05b52fc5310ed4205a36deba0dcac5f44fd90
Leon Tan <leon.arian.tan@gmail.com>
2025-07-09 22:59:44 +0200

    WIP working on the client

--- luct-client/src/lib.rs
+++ luct-client/src/lib.rs
@@ -1,1 +1,26 @@
+use luct_core::CtLog;
+use std::sync::Arc;
+use thiserror::Error;
+use url::Url;
 
+pub struct CtLogClient<C> {
+    log: CtLog,
+    client: C,
+}
+
+pub trait Client {
+    fn get(&self, url: &Url, params: &(&str, &str)) -> Result<String, ClientError>;
+
+    // TODO: Post
+}
+
+pub struct DynClient(Arc<dyn Client>);
+
+impl Client for DynClient {
+    fn get(&self, url: &Url, params: &(&str, &str)) -> Result<String, ClientError> {
+        self.0.get(url, params)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Error)]
+pub enum ClientError {}
