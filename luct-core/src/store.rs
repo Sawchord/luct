0b7ea3df533e4a3263ad0ad95d5e8f43e24b88ab
Leon Tan <leon.arian.tan@gmail.com>
2025-07-19 18:39:19 +0200

    Improve docs in store

--- luct-core/src/store.rs
+++ luct-core/src/store.rs
@@ -4,28 +4,75 @@ use std::{
     sync::{Arc, RwLock},
 };
 
+/// Trait indicating that an object can be hased with respect to the CT protocol
+///
+/// This for now always refers to the Sha256 algorithm, but this might change in the future
 pub trait Hashable {
+    /// Hash the object
     fn hash(&self) -> HashOutput;
 }
 
+/// The [`Store`] trait is a basic key-value store trait
+///
+/// Note that there is no ACID requirement in the trait.
+/// In fact, there is no notion of deletion whatsoever.
 pub trait Store<K, V> {
+    /// Insert a value into the store
+    ///
+    /// # Arguments:
+    /// - `key`: the key associated with the value
+    /// - `value`: the value itself
     fn insert(&self, key: K, value: V);
+
+    /// Returns the value associated with `key` from the [`Store`]
+    ///
+    /// # Arguments:
+    /// - `key`: the key indexing the object
+    ///
+    /// # Returns:
+    /// - `Some(value)`, if the value exists
+    /// - `None` otherwise
     fn get(&self, key: &K) -> Option<V>;
+
+    /// Returns the number of elements in the [`Store`]
     fn len(&self) -> usize;
 
+    /// Returns `true`, if the store is empty
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
 }
 
+/// Extension to regular [`Stores`](Store), which have ordered keys
 pub trait OrderedStore<K: Ord, V>: Store<K, V> {
-    fn last(&self) -> Option<V>;
+    /// Returns the last element in the store
+    ///
+    /// The last element is the largest element with respect to the keys [`Ord`] implementation.
+    ///
+    /// # Returns
+    /// - `Some(key, value)` if the store is non-empty
+    /// - `None` otherwise
+    fn last(&self) -> Option<(K, V)>;
 }
 
+/// Extension to regular [`Stores`](Store), which use an index as a key
+///
+/// The main difference is, that the key is a [`u64`] and the store determines the key when inserting
 pub trait IndexedStore<V>: Store<u64, V> {
+    /// Insert a value into the store and return the index
+    ///
+    /// # Arguments:
+    /// - `value`: the value itself
+    ///
+    /// # Returns:
+    /// - the index of the new value. This is the key under which the value can later be retreived
     fn insert_indexed(&self, value: V) -> u64;
 }
 
+/// A non-persistent implementation of [`Store`]
+///
+/// This can be useful for testing, or in settings, in which the data should not be stored,
+/// for example when running as a command line tool in CI
 #[derive(Debug, Clone)]
 pub struct MemoryStore<K, V>(Arc<RwLock<BTreeMap<K, V>>>);
 
@@ -49,14 +96,14 @@ impl<K: Ord, V: Clone> Store<K, V> for MemoryStore<K, V> {
     }
 }
 
-impl<K: Ord, V: Clone> OrderedStore<K, V> for MemoryStore<K, V> {
-    fn last(&self) -> Option<V> {
+impl<K: Ord + Clone, V: Clone> OrderedStore<K, V> for MemoryStore<K, V> {
+    fn last(&self) -> Option<(K, V)> {
         self.0
             .read()
             .unwrap()
             .iter()
             .next_back()
-            .map(|(_, v)| v.clone())
+            .map(|(k, v)| (k.clone(), v.clone()))
     }
 }
 
@@ -65,7 +112,12 @@ impl<V: Clone> IndexedStore<V> for MemoryStore<u64, V> {
         let mut store = self.0.write().unwrap();
 
         let len = store.len() as u64;
-        store.insert(len, value);
+        let old = store.insert(len, value);
+
+        assert!(
+            old.is_none(),
+            "IndexedStore already contained a value at {len}"
+        );
 
         len
     }
