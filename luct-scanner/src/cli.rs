9d69ab8d381c50dbf6e5cfd82d77dd60f2738ecb
Leon Tan <leon.arian.tan@gmail.com>
2025-07-19 17:46:49 +0200

    Create a dedicated crate for the cli

--- luct-scanner/src/cli.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // TODO: Get the certificate chain of the request
-    // TODO: Scan for leads
-    // TODO: Investigate the leads and print the results
-}
---
+++ luct/src/main.rs
@@ -0,0 +1,7 @@
+// TODO: Get the certificate chain of the request
+// TODO: Scan for leads
+// TODO: Investigate the leads and print the results
+
+fn main() {
+    println!("Hello world");
+}
