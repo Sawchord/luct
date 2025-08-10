5bb862550aad03902ecd7405f8ac9f77bd650933
Leon Tan <leon.arian.tan@gmail.com>
2025-08-10 16:52:29 +0200

    WIP building server

---
+++ otlsp-server/src/config.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+/// Configure the OTLSP server
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub(crate) struct Config {
+    /// Route at which to mount the proxy
+    pub(crate) route: String,
+
+    /// Endpoint
+    pub(crate) endpoint: String,
+
+    /// List of URLs, which the connection can be forwarded to
+    pub(crate) enabled_urls: Vec<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            route: String::from("/"),
+            endpoint: String::from("0.0.0.0:3000"),
+            enabled_urls: vec![String::from("127.0.0.1:8080")],
+        }
+    }
+}
--- otlsp-server/src/main.rs
+++ otlsp-server/src/main.rs
@@ -1,2 +1,71 @@
+use crate::config::Config;
+use axum::{
+    Router,
+    body::Body,
+    extract::{Query, State, WebSocketUpgrade, ws::WebSocket},
+    response::Response,
+    routing::get,
+};
+use serde::Deserialize;
+use tokio::{io::AsyncReadExt, net::TcpStream, select};
+
+mod config;
+
+#[derive(Deserialize)]
+struct Destination {
+    to: String,
+}
+
 #[tokio::main]
-async fn main() {}
+async fn main() -> eyre::Result<()> {
+    pretty_env_logger::init();
+    let config = Config::default();
+
+    let listener = tokio::net::TcpListener::bind(config.endpoint.clone())
+        .await
+        .unwrap();
+
+    let router = Router::new()
+        .route(&config.route, get(handle_connection))
+        .with_state(config);
+
+    tracing::info!("Start serving requests");
+    axum::serve(listener, router).await.unwrap();
+    Ok(())
+}
+
+async fn handle_connection(
+    config: State<Config>,
+    destination: Query<Destination>,
+    ws: WebSocketUpgrade,
+) -> Response {
+    tracing::debug!("Received a new connection request to {:?}", destination.to);
+
+    if !config.enabled_urls.iter().any(|url| url == &destination.to) {
+        tracing::debug!("Connection request rejected since it does not target enabled URL");
+
+        return Response::builder()
+            .status(400)
+            .body(Body::from("Requested destination is not enabled"))
+            .unwrap();
+    }
+
+    let Ok(mut stream) = TcpStream::connect(&destination.to).await else {
+        return Response::builder()
+            .status(400)
+            .body(Body::from("Requested destination is not enabled"))
+            .unwrap();
+    };
+    tracing::debug!("TCP stream to target established");
+
+    ws.on_upgrade(async move |mut ws: WebSocket| {
+        let mut buf = [0; 1500];
+
+        loop {
+            select! {
+                data = ws.recv() => {todo!()},
+                read = stream.read(&mut buf) => {todo!()},
+            }
+        }
+    })
+}
