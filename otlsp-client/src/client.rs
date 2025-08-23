51f233dbd31e1c5119ec7ebf5bab39368963676a
Leon Tan <leon.arian.tan@gmail.com>
2025-08-23 21:16:02 +0200

    WIP setting up client

--- otlsp-client/src/client.rs
+++ otlsp-client/src/client.rs
@@ -1,33 +1,35 @@
 use crate::error::OtlspError;
-use http_body_util::Empty;
-use hyper::body::Bytes;
-//use hyper_rustls::{ConfigBuilderExt, HttpsConnectorBuilder};
-//use hyper_util::client::legacy::Client as HyperClient;
-use rustls::ClientConfig;
+use rustls::{ClientConfig, ClientConnection, RootCertStore, client::WebPkiServerVerifier};
+use rustls_pki_types::ServerName;
 use std::sync::Arc;
 use url::Url;
 
-pub struct Client {
-    tls_config: Arc<ClientConfig>,
-}
+pub struct Client {}
 
 impl Client {
-    pub fn new(proxy: Url) -> Result<Self, OtlspError> {
-        // let https = HttpsConnectorBuilder::new()
-        //     .with_provider_and_webpki_roots(Arc::new(rustls_rustcrypto::provider()))
-        //     .unwrap()
-        //     .https_or_http()
-        //     .enable_all_versions()
-        //     .build();
+    pub async fn new(proxy: Url) -> Result<Self, OtlspError> {
+        let server_name = ServerName::try_from(proxy.domain().unwrap())
+            .unwrap()
+            .to_owned();
 
-        // let config = ClientConfig::builder_with_provider(rustls_rustcrypto::provider().into())
-        //     .with_safe_default_protocol_versions()
-        //     .unwrap();
-        // .with_webpki_roots()
-        // .with_no_client_auth();
+        let config = ClientConfig::builder_with_provider(rustls_rustcrypto::provider().into())
+            .with_protocol_versions(&[&rustls::version::TLS13])
+            .unwrap()
+            .with_webpki_verifier(
+                WebPkiServerVerifier::builder_with_provider(
+                    RootCertStore {
+                        roots: webpki_roots::TLS_SERVER_ROOTS.to_vec(),
+                    }
+                    .into(),
+                    rustls_rustcrypto::provider().into(),
+                )
+                .build()
+                .unwrap(),
+            )
+            .with_no_client_auth();
 
-        // let client: HyperClient<_, Empty<Bytes>> =
-        //     HyperClient::builder(futures::ready!(())).build(https);
+        let mut conn = ClientConnection::new(Arc::new(config), server_name).unwrap();
+
         todo!()
     }
 }
--- otlsp-client/src/lib.rs
+++ otlsp-client/src/lib.rs
@@ -1,2 +1,4 @@
 mod client;
 mod error;
+
+pub use client::Client;
