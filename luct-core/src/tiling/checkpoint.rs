c13c594ee7122e5a49f2761e900509eefc70a77a
Leon Tan <leon.arian.tan@gmail.com>
2026-01-21 11:22:02 +0100

    Use own signed note signature parser

--- luct-core/src/tiling/checkpoint.rs
+++ luct-core/src/tiling/checkpoint.rs
@@ -3,13 +3,30 @@ use crate::{
     tree::{HashOutput, TreeHead},
 };
 use base64::{Engine, prelude::BASE64_STANDARD};
-use signed_note::Signature;
 use thiserror::Error;
+
+#[derive(Debug, Clone, PartialEq, PartialOrd, Error)]
+pub enum ParseCheckpointError {
+    #[error("No {field_name} contained in the note")]
+    MissingField { field_name: &'static str },
+
+    #[error("{field_name} could not be parsed")]
+    MalformedField { field_name: &'static str },
+
+    #[error("Unexpected extensions appended to the note. We only expect notes with 3 fields")]
+    UnexpectedExtensions,
+
+    #[error("The note contains no signatures.")]
+    NoSignatures,
+
+    #[error("The signature at index {index} is malformed")]
+    MalformedSignature { index: usize },
+}
 
 impl CtLog {
     pub fn validate_checkpoint(
         &self,
-        checkpoint: Checkpoint,
+        checkpoint: &Checkpoint,
     ) -> Result<(), SignatureValidationError> {
         todo!()
     }
@@ -74,15 +91,15 @@ impl Checkpoint {
         // Check that there is an empty line
         let separator = data.next().ok_or(ParseCheckpointError::NoSignatures)?;
         if !separator.is_empty() {
-            return Err(ParseCheckpointError::UnexpectedFields);
+            return Err(ParseCheckpointError::UnexpectedExtensions);
         }
 
         // Parse the signatures
         let signatures = data
             .enumerate()
             .map(|(index, signature)| {
-                Signature::from_bytes(signature.as_bytes())
-                    .map_err(|_| ParseCheckpointError::MalformedSignature { index })
+                Signature::from_str(signature)
+                    .ok_or(ParseCheckpointError::MalformedSignature { index })
             })
             .collect::<Result<Vec<_>, _>>()?;
         if signatures.is_empty() {
@@ -100,22 +117,30 @@ impl Checkpoint {
     // TODO: `as_string` function and roundtrip test
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Error)]
-pub enum ParseCheckpointError {
-    #[error("No {field_name} contained in the note")]
-    MissingField { field_name: &'static str },
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Signature {
+    name: String,
+    id: [u8; 4],
+    body: Vec<u8>,
+}
 
-    #[error("{field_name} could not be parsed")]
-    MalformedField { field_name: &'static str },
+impl Signature {
+    fn from_str(data: &str) -> Option<Self> {
+        let mut data = data.strip_prefix("— ")?.split(" ");
+        let name = data.next()?.to_string();
 
-    #[error("Unexpected fields appended to the note. We only expect notes with 3 fields")]
-    UnexpectedFields,
+        let mut data = BASE64_STANDARD.decode(data.next()?).ok()?;
+        if data.len() < 4 {
+            return None;
+        }
 
-    #[error("The note contains no signatures.")]
-    NoSignatures,
+        let body = data.split_off(4);
+        let id: [u8; 4] = data.try_into().unwrap();
 
-    #[error("The signature at index {index} is malformed")]
-    MalformedSignature { index: usize },
+        Some(Self { name, id, body })
+    }
+
+    // TODO: `as_string` function
 }
 
 #[cfg(test)]
@@ -125,11 +150,27 @@ mod tests {
     const ARCHE2026H1_CHECKPOINT: &str =
         include_str!("../../../testdata/arche2026h1-signed-note.txt");
 
+    const ARCHE2026H1: &str = "
+    {
+          \"description\": \"Google 'Arche2026h1' log\",
+          \"log_id\": \"J+sqNJffaHpkC2Q4TkhW/Nyj6H+NzWbzTtbxvkKB7fw=\",
+          \"key\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEZ+3YKoZTMruov4cmlImbk4MckBNzEdCyMuHlwGgJ8BUrzFLlR5U0619xDDXIXespkpBgCNVQAkhMTTXakM6KMg==\",
+          \"url\": \"https://arche2026h1.staging.ct.transparency.dev/\",
+          \"tile_url\": \"https://storage.googleapis.com/static-ct-staging-arche2026h1-bucket/\",
+          \"mmd\": 60
+        }
+    ";
+
     #[test]
     fn parse_and_validate_checkpoint() {
         let checkpoint = Checkpoint::parse_checkpoint(ARCHE2026H1_CHECKPOINT).unwrap();
 
         assert_eq!(checkpoint.origin, "arche2026h1.staging.ct.transparency.dev");
         assert_eq!(checkpoint.tree_size, 1822167730);
+
+        let config = serde_json::from_str(ARCHE2026H1).unwrap();
+        let log = CtLog::new(config);
+
+        log.validate_checkpoint(&checkpoint).unwrap();
     }
 }
