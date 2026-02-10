f67fc1a7fa6f7fa0cace7efe912065bc005ca3bc
Leon Tan <leon.arian.tan@gmail.com>
2026-02-10 17:23:01 +0100

    Fix local store implementation

--- luct-extension/luct-extension/src/store.rs
+++ luct-extension/luct-extension/src/store.rs
@@ -28,6 +28,37 @@ impl<K: StringStoreKey, V> BrowserStore<K, V> {
     fn key_from_str(&self, key: &str) -> Option<K> {
         K::deserialize_key(&key[self.prefix.len() + 1..])
     }
+
+    fn count_key(&self) -> String {
+        format!("{}#count", self.prefix)
+    }
+
+    fn get_count(&self) -> usize {
+        self.storage
+            .get_item(&self.count_key())
+            .expect("Failed to retrieve count")
+            .unwrap_or("0".to_string())
+            .parse()
+            .expect("Count contains non integer value")
+    }
+
+    fn inc_count(&self) {
+        let count: usize = self.get_count();
+
+        self.storage
+            .set_item(&self.count_key(), &(count + 1).to_string())
+            .expect("Failed to set count");
+    }
+
+    fn last_key(&self) -> String {
+        format!("{}#last", self.prefix)
+    }
+
+    fn set_last(&self, key: &str) {
+        self.storage
+            .set_item(&self.last_key(), key)
+            .expect("Failed to set last value")
+    }
 }
 
 impl<K: StringStoreKey, V: StringStoreValue> Store<K, V> for BrowserStore<K, V> {
@@ -35,9 +66,25 @@ impl<K: StringStoreKey, V: StringStoreValue> Store<K, V> for BrowserStore<K, V> 
         let key = self.get_key_string(&key);
         let val = value.serialize_value();
 
+        match self
+            .storage
+            .get_item(&key)
+            .expect("Failed to retrive value from local store")
+        {
+            Some(old_val) if old_val == val => return,
+            Some(old_val) => panic!(
+                "Tried to overwrite value {} with {}. This is a bug",
+                old_val, val
+            ),
+            None => (),
+        };
+
         self.storage
             .set_item(&key, &val)
             .expect("Failed to insert value into local store");
+
+        self.inc_count();
+        self.set_last(&key);
     }
 
     fn get(&self, key: &K) -> Option<V> {
@@ -50,9 +97,7 @@ impl<K: StringStoreKey, V: StringStoreValue> Store<K, V> for BrowserStore<K, V> 
     }
 
     fn len(&self) -> usize {
-        self.storage
-            .length()
-            .expect("Failed to retreive store length") as usize
+        self.get_count()
     }
 }
 
@@ -60,9 +105,8 @@ impl<K: StringStoreKey + Ord, V: StringStoreValue> OrderedStore<K, V> for Browse
     fn last(&self) -> Option<(K, V)> {
         let key = self
             .storage
-            .key(self.len() as u32 - 1)
-            .expect("Failed to retreive key index of last element")?;
-
+            .get_item(&self.last_key())
+            .expect("Failed to retrieve last key")?;
         let val = self
             .storage
             .get_item(&key)
