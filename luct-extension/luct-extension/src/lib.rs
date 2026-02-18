6b0144c13ca0fab2300a24993ffa27934ce000f8
Leon Tan <leon.arian.tan@gmail.com>
2026-02-18 13:46:06 +0100

    Use console_error_panic_hook

--- luct-extension/luct-extension/src/lib.rs
+++ luct-extension/luct-extension/src/lib.rs
@@ -30,6 +30,8 @@ extern "C" {
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
     tracing_wasm::set_as_global_default_with_config(
         WASMLayerConfigBuilder::default()
             .set_max_level(Level::DEBUG)
