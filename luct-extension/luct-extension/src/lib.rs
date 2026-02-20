f90532a2d96e1fb68c98fa5771ec69ff5a9eed65
Leon Tan <leon.arian.tan@gmail.com>
2026-02-20 11:19:19 +0100

    Export Report API to JavaScript

--- luct-extension/luct-extension/src/lib.rs
+++ luct-extension/luct-extension/src/lib.rs
@@ -1,11 +1,12 @@
 //! Wrapper around [`Scanner`](CtScanner) to be used in a javascript environment.
 
 use crate::store::BrowserStore;
+use chrono::DateTime;
 use js_sys::{Array, Uint8Array};
 use luct_client::{deduplication::RequestDeduplicationClient, reqwest::ReqwestClient};
 use luct_core::{CertificateChain, log_list::v3::LogList, v1::SignedCertificateTimestamp};
 use luct_scanner::{
-    Conclusion as CtConclusion, Lead as CtLead, LeadResult as CtLeadResult, LogBuilder,
+    Conclusion as CtConclusion, Lead as CtLead, LeadResult as CtLeadResult, LogBuilder, Report,
     Scanner as CtScanner, Validated,
 };
 use std::sync::Arc;
@@ -13,6 +14,7 @@ use tracing::Level;
 use tracing_wasm::WASMLayerConfigBuilder;
 use url::Url;
 use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
+use web_time::SystemTime;
 
 mod store;
 
@@ -80,17 +82,53 @@ impl Scanner {
     }
 
     #[wasm_bindgen]
+    pub async fn collect_report(
+        &self,
+        url: String,
+        leads: Array,
+    ) -> Result<Option<JsValue>, String> {
+        // Check that this is not a recursion
+        if self.is_recursion(&url)? {
+            tracing::trace!("Skipping request to log itself to prevent recursion");
+            return Ok(None);
+        }
+
+        // Parse the certificate
+        let cert_chain_bytes = leads
+            .to_vec()
+            .into_iter()
+            .map(|value| Uint8Array::from(value).to_vec())
+            .collect::<Vec<_>>();
+
+        let cert_chain =
+            CertificateChain::from_der_chain(&cert_chain_bytes).map_err(|err| err.to_string())?;
+
+        // Generate the report
+        let report = self
+            .0
+            .collect_report(Arc::new(cert_chain))
+            .await
+            .map_err(|err| err.to_string())?;
+
+        let report = serde_wasm_bindgen::to_value(&report).map_err(|err| format!("{err}"))?;
+
+        Ok(Some(report))
+    }
+
+    #[wasm_bindgen]
+    pub fn evaluate_report(report: JsValue) -> Result<(), String> {
+        let report: Report =
+            serde_wasm_bindgen::from_value(report).map_err(|err| format!("{err}"))?;
+
+        let now = DateTime::from(SystemTime::now());
+        report.evaluate_policy(now)?;
+
+        Ok(())
+    }
+
+    #[wasm_bindgen]
     pub fn collect_leads(&self, url: String, leads: Array) -> Result<Vec<Lead>, String> {
-        let url = Url::parse(&url).map_err(|err| format!("{err}"))?;
-        if self.0.logs().any(|log| {
-            log.config().url().domain() == url.domain()
-                || log
-                    .config()
-                    .tile_url()
-                    .as_ref()
-                    .map(|fetch_url| fetch_url.domain())
-                    == Some(url.domain())
-        }) {
+        if self.is_recursion(&url)? {
             log("Skipping request to log itself to prevent recursion");
             return Ok(vec![]);
         }
@@ -119,6 +157,25 @@ impl Scanner {
     pub async fn investigate_lead(&self, lead: &Lead) -> LeadResult {
         //log("investigating lead");
         LeadResult(self.0.investigate_lead(&lead.0).await)
+    }
+
+    /// Check that we are not requesting from a URL that is the log itself
+    ///
+    /// This is necessary as in the browser, the calls to the logs go through the same
+    /// security context and will be intercepted by the browser
+    fn is_recursion(&self, url: &str) -> Result<bool, String> {
+        let url = Url::parse(url).map_err(|err| format!("{err}"))?;
+        let is_recusion = self.0.logs().any(|log| {
+            log.config().url().domain() == url.domain()
+                || log
+                    .config()
+                    .tile_url()
+                    .as_ref()
+                    .map(|fetch_url| fetch_url.domain())
+                    == Some(url.domain())
+        });
+
+        Ok(is_recusion)
     }
 }
 
