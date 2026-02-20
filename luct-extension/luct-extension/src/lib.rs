2db8f5e066e09944291f0a0d10d5736095aefde9
Leon Tan <leon.arian.tan@gmail.com>
2026-02-20 19:14:17 +0100

    Remove leads API from luct-extension

--- luct-extension/luct-extension/src/lib.rs
+++ luct-extension/luct-extension/src/lib.rs
@@ -5,10 +5,7 @@ use chrono::DateTime;
 use js_sys::{Array, Uint8Array};
 use luct_client::{deduplication::RequestDeduplicationClient, reqwest::ReqwestClient};
 use luct_core::{CertificateChain, log_list::v3::LogList, v1::SignedCertificateTimestamp};
-use luct_scanner::{
-    Conclusion as CtConclusion, Lead as CtLead, LeadResult as CtLeadResult, LogBuilder, Report,
-    Scanner as CtScanner, SctReport, Validated,
-};
+use luct_scanner::{LogBuilder, Report, Scanner as CtScanner, SctReport, Validated};
 use std::sync::Arc;
 use tracing::Level;
 use tracing_wasm::WASMLayerConfigBuilder;
@@ -140,39 +137,6 @@ impl Scanner {
         Ok(())
     }
 
-    #[wasm_bindgen]
-    pub fn collect_leads(&self, url: String, leads: Array) -> Result<Vec<Lead>, String> {
-        if self.is_recursion(&url)? {
-            log("Skipping request to log itself to prevent recursion");
-            return Ok(vec![]);
-        }
-
-        let cert_chain_bytes = leads
-            .to_vec()
-            .into_iter()
-            .map(|value| Uint8Array::from(value).to_vec())
-            .collect::<Vec<_>>();
-
-        let cert_chain =
-            CertificateChain::from_der_chain(&cert_chain_bytes).map_err(|err| format!("{err}"))?;
-        //log(&format!("{cert_chain:?}"));
-
-        let leads = self
-            .0
-            .collect_leads(Arc::new(cert_chain))
-            .map_err(|err| format!("{err}"))?;
-        //log(&format!("{leads:?}"));
-
-        //log("collected leads");
-        Ok(leads.into_iter().map(Lead).collect())
-    }
-
-    #[wasm_bindgen]
-    pub async fn investigate_lead(&self, lead: &Lead) -> LeadResult {
-        //log("investigating lead");
-        LeadResult(self.0.investigate_lead(&lead.0).await)
-    }
-
     /// Check that we are not requesting from a URL that is the log itself
     ///
     /// This is necessary as in the browser, the calls to the logs go through the same
@@ -190,64 +154,5 @@ impl Scanner {
         });
 
         Ok(is_recusion)
-    }
-}
-
-#[wasm_bindgen]
-pub struct Lead(CtLead);
-
-#[wasm_bindgen]
-impl Lead {
-    #[wasm_bindgen]
-    pub fn description(&self) -> String {
-        format!("{}", self.0)
-    }
-}
-
-#[wasm_bindgen]
-pub struct LeadResult(CtLeadResult);
-
-#[wasm_bindgen]
-impl LeadResult {
-    #[wasm_bindgen]
-    pub fn conclusion(&self) -> Option<Conclusion> {
-        match &self.0 {
-            CtLeadResult::Conclusion(conclusion) => Some(Conclusion(conclusion.clone())),
-            CtLeadResult::FollowUp(_) => None,
-        }
-    }
-
-    #[wasm_bindgen]
-    pub fn follow_up(self) -> Vec<Lead> {
-        match self.0 {
-            CtLeadResult::Conclusion(_) => vec![],
-            CtLeadResult::FollowUp(leads) => leads.into_iter().map(Lead).collect(),
-        }
-    }
-}
-
-#[wasm_bindgen]
-pub struct Conclusion(CtConclusion);
-
-#[wasm_bindgen]
-impl Conclusion {
-    #[wasm_bindgen]
-    pub fn description(&self) -> String {
-        format!("{}", self.0)
-    }
-
-    #[wasm_bindgen]
-    pub fn is_safe(&self) -> bool {
-        matches!(self.0, CtConclusion::Safe(_))
-    }
-
-    #[wasm_bindgen]
-    pub fn is_inconclusive(&self) -> bool {
-        matches!(self.0, CtConclusion::Inconclusive(_))
-    }
-
-    #[wasm_bindgen]
-    pub fn is_unsafe(&self) -> bool {
-        matches!(self.0, CtConclusion::Unsafe(_))
     }
 }
