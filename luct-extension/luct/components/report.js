5d99428b9073cda8722804e4f5382319bcee5f06
Leon Tan <leon.arian.tan@gmail.com>
2026-02-25 10:42:16 +0100

    Remove unused footer

--- luct-extension/luct/components/report.js
+++ luct-extension/luct/components/report.js
@@ -10,14 +10,15 @@ export default class Report extends HTMLElement {
         const anchor = document.createElement('div');
 
         anchor.innerHTML = `
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.4/css/bulma.min.css" integrity="sha512-yh2RE0wZCVZeysGiqTwDTO/dKelCbS9bP2L94UvOFtl/FKXcNAje3Y2oBg/ZMZ3LS1sicYk4dYVGtDex75fvvA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            
             <div class="card">
                 <header class="card-header">
                     <p class="card-header-title">${this.url}</p>
                     <button class="card-header-icon" aria-label="more options">
                         <span class="icon">
-                            <i class="fa-solid fa-angle-down" aria-hidden="true"></i>
+                            <i class="fa fa-angle-down" aria-hidden="true"></i>
                         </span>
                     </button>
                 </header>
@@ -38,11 +39,6 @@ export default class Report extends HTMLElement {
                         ${JSON.stringify(this.report)}
                     </div>
                 </div>
-                <footer class="card-footer">
-                    <a href="#" class="card-footer-item">Save</a>
-                    <a href="#" class="card-footer-item">Edit</a>
-                    <a href="#" class="card-footer-item">Delete</a>
-                </footer>
             </div>
         `;
 
