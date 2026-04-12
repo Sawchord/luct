1df3999e115f8dc1e9ed22ef56e8e871e783f086
Leon Tan <leon.arian.tan@gmail.com>
2026-04-12 17:08:19 +0200

    Remove console_log helper

--- otlsp-client/src/lib.rs
+++ otlsp-client/src/lib.rs
@@ -1,17 +1,5 @@
 mod browser;
 mod client;
 mod error;
-use wasm_bindgen::prelude::wasm_bindgen;
 
 pub use client::OtlspClientBuilder;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    pub(crate) fn log(s: &str);
-}
-
-macro_rules! console_log {
-    ($($t:tt)*) => (crate::log(&format_args!($($t)*).to_string()))
-}
-pub(crate) use console_log;
