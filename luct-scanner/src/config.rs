8fc4c46eaf6369c8c6837f0520ef844617d22406
Leon Tan <leon.arian.tan@gmail.com>
2026-04-16 22:03:44 +0200

    Add otlsp configuration

--- luct-scanner/src/config.rs
+++ luct-scanner/src/config.rs
@@ -1,8 +1,23 @@
 use serde::{Deserialize, Serialize};
+use url::Url;
+use web_time::Duration;
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ScannerConfig {
     pub(crate) validate_cert_chain: bool,
+
+    pub(crate) otlsp_url: Option<Url>,
+    pub(crate) otlsp_connection_timeout: Duration,
+}
+
+impl Default for ScannerConfig {
+    fn default() -> Self {
+        Self {
+            validate_cert_chain: Default::default(),
+            otlsp_url: Some(Url::parse("https://node.luct.dev/otlsp").unwrap()),
+            otlsp_connection_timeout: Duration::from_secs(30),
+        }
+    }
 }
 
 impl ScannerConfig {
@@ -10,8 +25,20 @@ impl ScannerConfig {
         Self::default()
     }
 
-    pub fn validate_cert_chain(mut self) -> Self {
+    pub fn validate_cert_chain(&self) -> bool {
+        self.validate_cert_chain
+    }
+
+    pub fn set_validate_cert_chain(mut self) -> Self {
         self.validate_cert_chain = true;
         self
+    }
+
+    pub fn otlsp_url(&self) -> &Option<Url> {
+        &self.otlsp_url
+    }
+
+    pub fn otlsp_connection_timeout(&self) -> &Duration {
+        &self.otlsp_connection_timeout
     }
 }
