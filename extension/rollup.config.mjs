47fac07f2fe2f347f3a6cd243764e8e4aa26d727
Leon Tan <leon.arian.tan@gmail.com>
2026-04-18 20:47:05 +0200

    Shrink svelte setup

--- extension/rollup.config.mjs
+++ extension/rollup.config.mjs
@@ -1,17 +1,10 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
 import livereload from "rollup-plugin-livereload";
 import serve from "rollup-plugin-serve";
-// import copy from "rollup-plugin-copy";
 import css from "rollup-plugin-css-only";
-// import fs from "node:fs";
-// import path from "node:path";
-// import posthtml from "posthtml";
-// import { hash } from "posthtml-hash";
 
 const PROD = !process.env.ROLLUP_WATCH;
-
 
 export default {
     input: "src/luct.js",
@@ -33,6 +26,5 @@ export default {
             port: 3000,
         }),
         !PROD && livereload({ watch: "luct" }),
-        PROD && terser(),
     ],
 };
