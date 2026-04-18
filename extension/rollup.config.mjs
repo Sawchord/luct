64cf485a7ac68f12c4720cb541518eb6f7a2e76e
Leon Tan <leon.arian.tan@gmail.com>
2026-04-18 20:41:11 +0200

    Get svelte to build

--- extension/rollup.config.mjs
+++ extension/rollup.config.mjs
@@ -1,20 +1,38 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+// import copy from "rollup-plugin-copy";
+import css from "rollup-plugin-css-only";
+// import fs from "node:fs";
+// import path from "node:path";
+// import posthtml from "posthtml";
+// import { hash } from "posthtml-hash";
+
+const PROD = !process.env.ROLLUP_WATCH;
+
 
 export default {
-    input: 'src/luct.js',
+    input: "src/luct.js",
     output: {
-        sourcemap: false,
-        format: 'iife',
-        name: 'sidebar',
-        file: 'luct/assets/bundle.js'
+        sourcemap: !PROD,
+        format: "iife",
+        name: "sidebar",
+        file: "luct/assets/bundle.js",
     },
     plugins: [
-        svelte(),
-        resolve({
-            browser: true,
-            dedupe: ['svelte']
+        svelte({
+            compilerOptions: { dev: !PROD },
         }),
-
-    ]
-}
+        css({ output: "bundle.css" }),
+        resolve(),
+        !PROD &&
+        serve({
+            contentBase: ["luct"],
+            port: 3000,
+        }),
+        !PROD && livereload({ watch: "luct" }),
+        PROD && terser(),
+    ],
+};
