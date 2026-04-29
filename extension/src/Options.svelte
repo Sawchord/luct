0c85aee9ea2ab5073f1c54a077140fac281e3dc5
Leon Tan <leon.arian.tan@gmail.com>
2026-04-29 13:04:20 +0200

    WIP implementing options

--- extension/src/Options.svelte
+++ extension/src/Options.svelte
@@ -1,14 +1,46 @@
+<script>
+    let settings = JSON.parse(window.localStorage.getItem("settings"));
+    let old_settings = settings;
+    console.log(settings);
+
+    function store_and_reload() {
+        console.log(settings);
+    }
+</script>
+
 <div class="card">
+    <header class="card-header">
+        <p class="card-header-title">Settings</p>
+        <button class="card-header-icon" aria-label="more options">
+            <span class="icon is-large">
+                <i class="fa fa-lg fa-cog" aria-hidden="true"></i>
+            </span>
+        </button>
+    </header>
+
     <div class="card-content">
+        <label class="checkbox">
+            <input
+                bind:checked={settings.validate_cert_chain}
+                type="checkbox"
+            />
+            Validate certificate chain
+        </label>
+
         <div class="field">
             <!-- svelte-ignore a11y-label-has-associated-control -->
             <label class="label">Oblivious TLS proxy</label>
             <label class="checkbox">
-                <input type="checkbox" />
+                <input bind:checked={settings.use_otlsp} type="checkbox" />
                 Use oblivious TLS proxy
             </label>
             <div class="control">
-                <input class="input" type="text" placeholder="Url" />
+                <input
+                    bind:value={settings.otlsp_url}
+                    class="input"
+                    type="text"
+                    placeholder=""
+                />
             </div>
             <p class="help">
                 Full url to the OTLSP endpoint. E.g.
@@ -20,7 +52,12 @@
             <!-- svelte-ignore a11y-label-has-associated-control -->
             <label class="label">STH freshness threshold (in seconds)</label>
             <div class="control">
-                <input class="input" type="text" placeholder="" />
+                <input
+                    bind:value={settings.sth_freshness_threshold}
+                    class="input"
+                    type="number"
+                    placeholder=""
+                />
             </div>
             <p class="help">
                 STHs younger than this are considered "fresh", older ones
@@ -32,7 +69,12 @@
             <!-- svelte-ignore a11y-label-has-associated-control -->
             <label class="label">STH update threshold (in seconds)</label>
             <div class="control">
-                <input class="input" type="text" placeholder="" />
+                <input
+                    bind:value={settings.sth_update_threshold}
+                    class="input"
+                    type="number"
+                    placeholder=""
+                />
             </div>
             <p class="help">
                 luCT will fetch a fresh STH for a log, if the existing one is
@@ -41,14 +83,16 @@
         </div>
 
         <label class="checkbox">
-            <input type="checkbox" />
+            <input bind:checked={settings.debug_output} type="checkbox" />
             Debug output
         </label>
 
         <footer class="card-footer">
             <div class="control">
-                <button class="button is-primary card-footer-item"
-                    >Save settings and reload extension</button
+                <button
+                    on:click={store_and_reload}
+                    class="button is-primary card-footer-item"
+                    >Save settings and reload</button
                 >
             </div>
         </footer>
--- luct-extension/src/config.rs
+++ luct-extension/src/config.rs
@@ -30,38 +30,40 @@ pub fn load_config() -> Result<ExtensionConfig, String> {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExtensionConfig {
-    #[serde(default = "default_true")]
+    #[serde(default = "default_false")]
     validate_cert_chain: bool,
 
-    #[serde(default = "otlsp_url")]
-    otlsp_url: Option<String>,
+    #[serde(default = "default_false")]
+    use_otlsp: bool,
 
-    #[serde(default = "sth_freshness_threshold")]
+    #[serde(default = "default_otlsp_url")]
+    otlsp_url: String,
+
+    #[serde(default = "default_sth_freshness_threshold")]
     sth_freshness_threshold: u64,
 
-    #[serde(default = "sth_update_threshold")]
+    #[serde(default = "default_sth_update_threshold")]
     sth_update_threshold: u64,
 
     #[serde(default = "default_false")]
     debug_output: bool,
 }
 
-fn otlsp_url() -> Option<String> {
-    // Some("https://node.luct.dev/otlsp".to_string())
-    None
+fn default_otlsp_url() -> String {
+    "https://node.luct.dev/otlsp".to_string()
 }
 
-fn sth_freshness_threshold() -> u64 {
+fn default_sth_freshness_threshold() -> u64 {
     24 * 60 * 60
 }
 
-fn sth_update_threshold() -> u64 {
+fn default_sth_update_threshold() -> u64 {
     8 * 60 * 60
 }
 
-fn default_true() -> bool {
-    true
-}
+// fn default_true() -> bool {
+//     true
+// }
 
 fn default_false() -> bool {
     false
@@ -71,12 +73,10 @@ impl TryFrom<&ExtensionConfig> for ScannerConfig {
     type Error = String;
 
     fn try_from(config: &ExtensionConfig) -> Result<Self, Self::Error> {
-        let otlsp_url = config
-            .otlsp_url
-            .as_ref()
-            .map(|url| Url::parse(url))
-            .transpose()
-            .map_err(|err| err.to_string())?;
+        let otlsp_url = match config.use_otlsp {
+            true => Some(Url::parse(&config.otlsp_url).map_err(|err| err.to_string())?),
+            false => None,
+        };
 
         let config = ScannerConfig::builder()
             .validate_cert_chain(config.validate_cert_chain)
