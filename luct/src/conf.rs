8d3179c29ed32b1679382fd6c0159a5775f51223
Leon Tan <leon.arian.tan@gmail.com>
2026-05-19 12:50:43 +0200

    WIP implement configuration

--- luct/src/conf.rs
+++ luct/src/conf.rs
@@ -1,6 +1,8 @@
 use config::{Config as Conf, Environment, File};
 use luct_scanner::ScannerConfig;
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
+use url::Url;
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub(crate) struct CliConfig {
@@ -60,6 +62,21 @@ impl TryFrom<&CliConfig> for ScannerConfig {
     type Error = String;
 
     fn try_from(config: &CliConfig) -> Result<Self, Self::Error> {
-        todo!()
+        let otlsp_url = config
+            .otlsp_url
+            .as_ref()
+            .map(|url| Url::parse(url))
+            .transpose()
+            .map_err(|err| err.to_string())?;
+
+        let config = ScannerConfig::builder()
+            .validate_cert_chain(config.validate_cert_chain)
+            .otlsp_url(otlsp_url)
+            .sth_freshness_threshold(Duration::from_secs(config.sth_freshness_threshold))
+            .sth_freshness_threshold(Duration::from_secs(config.sth_freshness_threshold))
+            .build()
+            .map_err(|err| err.to_string())?;
+
+        Ok(config)
     }
 }
