3a1cae27fc911806a08f946650d24d4addbeca27
Leon Tan <leon.arian.tan@gmail.com>
2026-05-28 16:16:32 +0200

    Factor out status icon

---
+++ extension/src/components/StatusIcon.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let status;
+
+    var icon = "";
+    var color = "";
+
+    switch (status) {
+        case "safe":
+            icon = "fa-check";
+            color = "has-text-success";
+            break;
+        default:
+            icon = "";
+            color = "";
+    }
+    icon = `fa ${icon}`;
+    color = `icon ${color}`;
+</script>
+
+<button class="card-header-icon">
+    <span class={color}>
+        <i class={icon} aria-hidden="true"></i>
+    </span>
+</button>
