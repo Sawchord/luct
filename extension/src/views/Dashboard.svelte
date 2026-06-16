66f5d7149fa421db602531b0bb3818e6f3620cb1
Leon Tan <leon.arian.tan@gmail.com>
2026-06-16 14:17:10 +0200

    Implement basic donut chart

--- extension/src/views/Dashboard.svelte
+++ extension/src/views/Dashboard.svelte
@@ -1,1 +1,13 @@
+<script>
+    import Donut from "../components/charts/Donut.svelte";
+
+    let data = [
+        ["A", 200],
+        ["B", 100],
+        ["C", 50],
+    ];
+</script>
+
 <b>TODO: Implement the dashboard page</b>
+
+<Donut {data} />
