6df1fda169755991662a92691eb63d4c4f96d7c4
Leon Tan <leon.arian.tan@gmail.com>
2026-06-16 14:44:24 +0200

    WIP setting up dashboard page

--- extension/src/views/Dashboard.svelte
+++ extension/src/views/Dashboard.svelte
@@ -1,5 +1,6 @@
 <script>
     import Donut from "../components/charts/Donut.svelte";
+    import Page from "../components/Page.svelte";
 
     let data = [
         ["A", 200],
@@ -8,6 +9,41 @@
     ];
 </script>
 
-<b>TODO: Implement the dashboard page</b>
+<Page>
+    <div slot="content">
+        <b>TODO: Implement the dashboard page</b>
 
-<Donut {data} />
+        <div class="columns">
+            <div class="column">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">Root CAs</p>
+                    </header>
+                    <div class="card-image">
+                        <Donut {data} />
+                    </div>
+                    <footer class="card-footer">
+                        <div class="content">
+                            <b>Total: 350</b>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">Root CAs</p>
+                    </header>
+                    <div class="card-image">
+                        <Donut {data} />
+                    </div>
+                    <footer class="card-footer">
+                        <div class="content">
+                            <b>Total: 350</b>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </div>
+</Page>
