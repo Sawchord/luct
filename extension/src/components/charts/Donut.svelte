fe131125a3e979f340e8d8d3a44e84ce3d31c9fd
Leon Tan <leon.arian.tan@gmail.com>
2026-06-16 14:17:10 +0200

    Implement basic donut chart

---
+++ extension/src/components/charts/Donut.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Chart from "chart.js/auto";
+    import { onMount } from "svelte/internal";
+
+    export let data;
+    let canvas;
+
+    const labels = data.map((val) => val[0]);
+    const values = data.map((val) => val[1]);
+
+    onMount(() => {
+        new Chart(canvas, {
+            type: "doughnut",
+            options: {
+                cutout: "75%",
+                plugins: {
+                    legend: {
+                        position: "bottom",
+                    },
+                },
+            },
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        data: values,
+                    },
+                ],
+            },
+        });
+    });
+</script>
+
+<canvas bind:this={canvas}></canvas>
