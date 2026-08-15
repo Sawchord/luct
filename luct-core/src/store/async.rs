48898b2adda674121698a278a7c5327ce224678c
Leon Tan <leon.arian.tan@gmail.com>
2026-08-15 18:07:49 +0200

    Extend async store to include all versions of sync store

--- luct-core/src/store/async.rs
+++ luct-core/src/store/async.rs
@@ -1,4 +1,5 @@
 use crate::store::StoreBase;
+use std::future::Future;
 
 pub trait AsyncStoreRead: StoreBase {
     /// Returns the value associated with `key` from the [`Store`](crate::store::Store)
@@ -36,3 +37,77 @@ pub trait AsyncStoreWrite: AsyncStoreRead {
 pub trait AsyncStore: AsyncStoreRead + AsyncStoreWrite {}
 
 impl<T> AsyncStore for T where T: AsyncStoreRead + AsyncStoreWrite {}
+
+/// Async version of [`OrderedStore`](crate::store::OrderedStore)
+pub trait AsyncOrderedStoreRead: AsyncStoreRead<Key: Ord> {
+    /// Returns the last element in the store
+    ///
+    /// The last element is the largest element with respect to the keys [`Ord`] implementation.
+    ///
+    /// # Returns
+    /// - `Some(key, value)` if the store is non-empty
+    /// - `None` otherwise
+    fn last(&self) -> impl Future<Output = Option<(Self::Key, Self::Value)>>;
+}
+
+pub trait AsyncOrderedStore: AsyncOrderedStoreRead + AsyncStoreWrite {}
+impl<T> AsyncOrderedStore for T where T: AsyncOrderedStoreRead + AsyncStoreWrite {}
+
+/// Async version of [`AppendableStore`](crate::store::AppendableStore)
+pub trait AsyncAppendableStore: AsyncOrderedStoreRead {
+    /// Insert a value into the store and return the index
+    ///
+    /// # Arguments:
+    /// - `value`: the value itself
+    ///
+    /// # Returns:
+    /// - the index of the new value. This is the key under which the value can later be retreived
+    fn append(&self, value: Self::Value) -> impl Future<Output = Self::Key>;
+}
+
+/// Async version of [`SearchableStore`](crate::store::SearchableStore)
+pub trait AsyncSearchableStoreRead: AsyncOrderedStoreRead {
+    /// Search for all entries in the store, that fulfill a certain predicate
+    ///
+    /// Note that the elements are being searched through in the order specified by [`Ord`] of key
+    ///
+    /// # Arguments
+    /// - `pred`: A predicate that has access to the key and value
+    ///
+    /// # Returns
+    /// - An array of key-value pairs, for which `pred` holds true
+    fn filter(
+        &self,
+        pred: impl FnMut(Self::Key, Self::Value) -> bool,
+    ) -> impl Future<Output = Vec<(Self::Key, Self::Value)>>;
+
+    fn find<'a>(
+        &'a self,
+        mut pred: impl FnMut(Self::Key, Self::Value) -> bool + 'a,
+    ) -> impl Future<Output = Option<(Self::Key, Self::Value)>> {
+        async move {
+            let mut found = false;
+
+            let vals = self
+                .filter(|key, value| {
+                    if !found && pred(key, value) {
+                        found = true;
+                        true
+                    } else {
+                        false
+                    }
+                })
+                .await;
+
+            if found {
+                assert_eq!(vals.len(), 1);
+                Some(vals.into_iter().next().unwrap())
+            } else {
+                None
+            }
+        }
+    }
+}
+
+pub trait AsyncSearchableStore: AsyncSearchableStoreRead + AsyncStoreWrite {}
+impl<T> AsyncSearchableStore for T where T: AsyncSearchableStoreRead + AsyncStoreWrite {}
